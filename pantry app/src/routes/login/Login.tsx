ccd58814290888856d367c42768d0263a9b4ba25
PlamenD <plamen.d@protonmail.com>
2024-12-15 15:30:32 +0200

    added basic react router library

---
+++ pantry app/src/routes/login/Login.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router'
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <nav>
+        <Link to='/'>Home</Link>
+      </nav>
+    </div>
+  )
+}
