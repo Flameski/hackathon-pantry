f022eb74741cffda8da57705df699a95b7ce280d
PlamenD <plamen.d@protonmail.com>
2024-12-15 22:45:17 +0200

    add axios for POST request

--- pantry app/src/components/AddItemsModal.tsx
+++ pantry app/src/components/AddItemsModal.tsx
@@ -13,23 +13,92 @@ import { ButtonsRow } from './styled/ButtonsRow'
 
 import { useForm, SubmitHandler } from 'react-hook-form'
 
+import axios from 'axios'
+
 interface AddItemsModalProps {
+  tableId: number | null
   open: boolean
   onClose: () => void
 }
 
 interface Inputs {
-  productName: string
-  productQuantity: string
+  name: string
+  quantity: number
   productUnit: string
-  productExpiryDate: string
-  productAmount: number
-  productDescription: string
+  expiryDate: string
+  amount: number
+  description: string
 }
 
-export const AddItemsModal = ({ open, onClose }: AddItemsModalProps) => {
+export const AddItemsModal = ({
+  open,
+  onClose,
+  tableId,
+}: AddItemsModalProps) => {
   const { register, handleSubmit } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<Inputs> = (data) => handleAddItem(data)
+
+  const handleAddItem = async (data: Inputs) => {
+    let quantityType
+    switch (data.productUnit) {
+      case 'kg':
+        quantityType = 1
+        break
+      case 'l':
+        quantityType = 2
+        break
+      case 'g':
+        quantityType = 1
+        break
+      case 'count':
+        quantityType = 0
+        break
+    }
+
+    data.quantity = Number(data.quantity)
+
+    // console.log({ ...data, tableId, quantityType })
+    // const rawResponse = await fetch('http://pantry.tryasp.net/product', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     Accept: 'application/json',
+    //   },
+    //   body: { ...data, tableId, quantityType },
+    // })
+    // const content = await rawResponse.json()
+
+    axios
+      .post(
+        'http://pantry.tryasp.net/product',
+        {
+          ...data,
+          tableId,
+          quantityType,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers':
+              'Origin, Content-Type, X-Auth-Token',
+            'Access-Control-Allow-Methods':
+              'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+          },
+        }
+      )
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    onClose()
+
+    // console.log(content)
+  }
+
   return (
     <Dialog open={open} onClose={onClose} maxWidth='lg'>
       <Box width={800} p={4}>
@@ -41,7 +110,7 @@ export const AddItemsModal = ({ open, onClose }: AddItemsModalProps) => {
                 id='product-name'
                 placeholder='Product name'
                 fullWidth
-                {...register('productName')}
+                {...register('name')}
               />
             </Grid>
             <Grid size={10}>
@@ -50,7 +119,7 @@ export const AddItemsModal = ({ open, onClose }: AddItemsModalProps) => {
                 id='product-quantity'
                 placeholder='Product quantity'
                 fullWidth
-                {...register('productQuantity')}
+                {...register('quantity')}
               />
             </Grid>
             <Grid size={2}>
@@ -69,11 +138,7 @@ export const AddItemsModal = ({ open, onClose }: AddItemsModalProps) => {
             </Grid>
             <Grid size={8}>
               <InputLabel htmlFor='expiry-date'>Expiry date</InputLabel>
-              <Input
-                id='expiry-date'
-                fullWidth
-                {...register('productExpiryDate')}
-              />
+              <Input id='expiry-date' fullWidth {...register('expiryDate')} />
             </Grid>
             <Grid size={4}>
               <InputLabel htmlFor='amount-of-items'>
@@ -84,7 +149,7 @@ export const AddItemsModal = ({ open, onClose }: AddItemsModalProps) => {
                 type='number'
                 placeholder='Amount'
                 fullWidth
-                {...register('productAmount')}
+                {...register('amount')}
               />
             </Grid>
             <Grid size={12}>
@@ -93,7 +158,7 @@ export const AddItemsModal = ({ open, onClose }: AddItemsModalProps) => {
                 rows={4}
                 placeholder='Description'
                 fullWidth
-                {...register('productDescription')}
+                {...register('description')}
               />
             </Grid>
           </Grid>
